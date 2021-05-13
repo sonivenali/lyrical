d7835e9d2c6a1020ad70acdd44958cc447e51785
Venali Soni <sonivenali.vs@gmail.com>
2021-05-14 00:33:22 +0530

    Created Services

--- lib/service/lyrical_service.dart
+++ lib/service/lyrical_service.dart
@@ -1,9 +1,12 @@
+import 'package:dio/dio.dart';
 
-
-class LyricalService{
+class LyricalService {
   final searchApi = "api.genius.com/search";
 
-  search(){
-    final response =
+  search(String query) async {
+    final response = await Dio().get(searchApi, queryParameters: {"q": query},
+        options: Options(headers: {
+          "Authorization": "Bearer qiwqeOKYgBwsLJ3ybcJG3ycDkIcileq-i-SV605FtuVlZjYpktokeftOQvpen1Yx"
+        }));
   }
 }
